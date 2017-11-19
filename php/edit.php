09dc3697b74e77f52f0832edf12c4b7b1a940b2f
yonis1 <yonismu@hotmail.com>
2017-11-19 15:42:24 +0200

    updt

---
+++ php/edit.php
@@ -0,0 +1,19 @@
+<?php
+require ('connection.php');
+session_start();
+$page='edit';
+include('include\header.php');
+?>
+
+<div class="col-md-12">
+  <h2>Muokkaa</h2>
+
+  <body>
+
+
+  </body>
+
+
+  <?php
+    include('include\footer.php');
+  ?>
--- php/index.php
+++ php/index.php
@@ -3,5 +3,16 @@
   session_start();
   $page='koti';
   include('include\header.php');
+  ?>
+
+  <div class="col-md-12">
+    <h2>Koti</h2>
+
+    <body>
+
+    </body>
+
+
+  <?php
   include('include\footer.php');
 ?>
